757f006d810de75218edb1fa03b635bd05d82eff
nonyx777 <nahomretebo@gmail.com>
2023-09-01 10:58:12 +0300

    WATCHLIST tests pass

--- spec/models/watchlist_spec.rb
+++ spec/models/watchlist_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Watchlist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has to validate factory' do
+    expect(create(:watchlist)).to be_valid
+  end
+
+  it {is_expected.to(validate_presence_of(:user_id))}
+  it {is_expected.to(validate_presence_of(:product_id))}
 end
