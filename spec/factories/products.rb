a3d20927b30355f97dd8974ff47d13269de7ad5a
nonyx777 <nahomretebo@gmail.com>
2023-09-01 10:36:47 +0300

    PRODUCT test passes

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :product do
-    
+    seller_id {create(:user).id}
+    buyer_id {create(:user).id}
+    category_id {create(:category).id}
+    name {Faker::Name.name}
+    starting_price {50.0}
+    description {Faker::Lorem.paragraph}
+    image {Faker::Avatar.image}
+    status {"available"}
+    location {Faker::Address.full_address}
+    bidding_end_time {Faker::Date.between(from: Date.today, to: 1.week.from_now)}
   end
 end
