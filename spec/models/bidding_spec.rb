0db07ff0192a5e834113633089ddb32a95208d4f
nonyx777 <nahomretebo@gmail.com>
2023-09-01 10:50:19 +0300

    BIDDING tests pass

--- spec/models/bidding_spec.rb
+++ spec/models/bidding_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Bidding, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has to validate factory' do
+    expect(create(:bidding)).to be_valid
+  end
+
+  it {is_expected.to(validate_presence_of(:user_id))}
+  it {is_expected.to(validate_presence_of(:product_id))}
+  it {is_expected.to(validate_presence_of(:bidding_price))}
 end
