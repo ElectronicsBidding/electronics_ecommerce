7eeff395ce627b501cade5327e2c8baed7d08333
hewansirak <hewansg@gmail.com>
2023-09-02 07:33:43 +0300

    used common controller and common examples for watchlists

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,51 +1,7 @@
 class WatchlistsController < ApplicationController
-  before_action :set_watchlist, only: %i[ show update destroy ]
-
-  # GET /watchlists
-  def index
-    @watchlists = Watchlist.all
-
-    render json: @watchlists
-  end
-
-  # GET /watchlists/1
-  def show
-    render json: @watchlist
-  end
-
-  # POST /watchlists
-  def create
-    @watchlist = Watchlist.new(watchlist_params)
-
-    if @watchlist.save
-      render json: @watchlist, status: :created, location: @watchlist
-    else
-      render json: @watchlist.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /watchlists/1
-  def update
-    if @watchlist.update(watchlist_params)
-      render json: @watchlist
-    else
-      render json: @watchlist.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /watchlists/1
-  def destroy
-    @watchlist.destroy
-  end
-
+include Common
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_watchlist
-      @watchlist = Watchlist.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def watchlist_params
-      params.fetch(:watchlist, {})
+    def model_params
+      params.require(:payload).permit(:user_id, :product_id)
     end
 end
