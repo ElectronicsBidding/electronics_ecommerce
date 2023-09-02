86c0fa36943cafe7c9c018efe0b7dc88c1f18dca
hewansirak <hewansg@gmail.com>
2023-09-02 07:15:49 +0300

    used common controller and common examples for products

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :seller_id, :buyer_id, :category_id, :name, :starting_price, :description, :image, :status, :location, :bidding_end_time
 end
